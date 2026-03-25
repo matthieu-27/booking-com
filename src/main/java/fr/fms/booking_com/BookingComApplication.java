5a6174a831be9006c387fd641d2fc1c8bef03f87
matthieu-27 <matthieu27@pm.me>
2026-03-25 15:41:11 +0100

    build: generate project

---
+++ src/main/java/fr/fms/booking_com/BookingComApplication.java
@@ -0,0 +1,13 @@
+package fr.fms.booking_com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingComApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingComApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/fms/booking_com/BookingComApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.fms.booking_com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingComApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
