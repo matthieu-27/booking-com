85c7385c259395d73863cbdc17cb867dca541dcd
matthieu-27 <matthieu27@pm.me>
2026-03-26 10:08:55 +0100

    fix: remove setter setId

--- src/main/java/fr/fms/booking_com/entities/Room.java
+++ src/main/java/fr/fms/booking_com/entities/Room.java
@@ -31,10 +31,6 @@ public class Room implements Serializable {
         return this.id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return this.name;
     }
